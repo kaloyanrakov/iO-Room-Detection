3d41e2937c337a359e196fd732b8c08e6cdb9fa8
Robert Figaroa <robertprogramming@hotmail.com>
2024-11-06 12:36:34 +0100

    Initial commit, added HttpDataSender and MacAddressFetcher

---
+++ RaspberryBackend/src/main/java/nl/fontys/s3/HttpDataSender.java
@@ -0,0 +1,43 @@
+package nl.fontys.s3;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpDataSender {
+    public static void main(String[] args) {
+        String serverUrl = "http://localhost:8080/pio";
+        String macAddress = MacAddressFetcher.getMacAddress();
+
+        if (macAddress == null) {
+            System.out.println("Mac Address is null");
+            return;
+        }
+
+        String jsonPayload = "{ \"macAddress\": \"" + macAddress + "\" }";
+
+        try {
+            URL url = new URL(serverUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+
+            // Write JSON payload to request
+            try (OutputStream outputStream = connection.getOutputStream()) {
+                byte[] input = jsonPayload.getBytes("utf-8");
+
+                outputStream.write(input, 0, input.length);
+            }
+
+            int responseCode = connection.getResponseCode();
+
+            System.out.println("Response Code: " + responseCode);
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ RaspberryBackend/src/main/java/nl/fontys/s3/Main.java
@@ -0,0 +1,23 @@
+package nl.fontys.s3;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        VideoCapture camera = new VideoCapture(0);
+//        Mat frame = new Mat();
+//        if (camera.read(frame)) {
+//            System.out.println("Captured frame with OpenCV");
+//        }
+//        camera.release();
+
+
+    }
+}
