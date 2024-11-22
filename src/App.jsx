d2ee4eece86f0fc33a67c64325c37f4bad52c96a
Kaloyan Rakov <i507614@fhict.nl>
2024-11-22 12:57:48 +0100

    Update Room Page Added
    Update Room Page Added, similar to the Add room page and according to the latest figma

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Route, Routes, BrowserRouter as Router} from "react-router-dom";
 import AllRoomsPage from './components/Pages/AllRoomsPage';
 import RoomInfoPage from './components/Pages/RoomInfoPage';
 import AddRoomPage from './components/Pages/AddRoomPage';
+import UpdateRoomPage from "./components/Pages/UpdateRoomPage"
 
 import './App.css'
 
@@ -12,6 +13,7 @@ function App() {
               <Route path="/rooms" element={<AllRoomsPage />} />
               <Route path="/roominfo" element={<RoomInfoPage />} />
               <Route path="/add-room" element={<AddRoomPage />} />
+              <Route path="/update-room" element={<UpdateRoomPage />} />
           </Routes>
       </Router>
   )
