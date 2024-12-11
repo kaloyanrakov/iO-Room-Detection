8c671f762a8bb5cd7dd31fa679e1f001d3d597e9
Kaloyan Rakov <i507614@fhict.nl>
2024-12-11 22:24:28 +0100

    Pagination Implemented
    Pagination Implemented with styling

--- src/api/fetchRooms.js
+++ src/api/fetchRooms.js
@@ -1,13 +1,37 @@
 import axios from 'axios';
 
-const fetchRooms = async () => {
+const fetchRooms = async (pageIndex, pageSize) => {
     try {
-        const response = await axios.get('http://localhost:8080/rooms?pageSize=12');
+        const response = await axios.get('http://localhost:8080/rooms', {
+            params: {
+                pageIndex: pageIndex,
+                pageSize: pageSize,
+            }
+        });
         console.log('Fetched rooms:', response.data);
-        return response.data.rooms;
+        return {
+            rooms: response.data.rooms,
+            totalRooms: response.data.totalRooms, // Assuming the backend returns the total number of rooms
+        };
     } catch (error) {
         console.error('Error fetching rooms:', error);
         throw error;
     }
 };
+
+
+const fetchAllRooms = async () => {
+    try {
+        const response = await axios.get('http://localhost:8080/rooms');  // No pagination params
+        console.log('Fetched all rooms:', response.data);
+        return {
+            rooms: response.data.rooms,
+            totalRooms: response.data.totalRooms,  // Assuming the backend returns the total number of rooms
+        };
+    } catch (error) {
+        console.error('Error fetching all rooms:', error);
+        throw error;
+    }
+};
+
 export default fetchRooms;
